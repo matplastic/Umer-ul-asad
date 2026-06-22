910b02825bea97d93d3291011ef8769052c7cce3
matplastic <sanaasim76765@gmail.com>
2026-06-22 18:19:23 +0400

    fix: hardcode firebase config

--- src/src/lib/firebase.ts
+++ src/src/lib/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: "AIzaSyADtkZJvDdrtbebPJw7nZYERHy39irZj_c",
+  authDomain: "materp-fad7f.firebaseapp.com",
+  projectId: "materp-fad7f",
+  storageBucket: "materp-fad7f.firebasestorage.app",
+  messagingSenderId: "261208029337",
+  appId: "1:261208029337:web:11125823e0226c77432008"
 };
 
 const app = initializeApp(firebaseConfig);
